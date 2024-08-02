bf619f335b662facaa1cf669a35d58cec1e06cc7
serhii.kotenko <serega.kotenko@gmail.com>
2024-08-02 14:25:11 +0300

    refactored for Versel

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req: express.Request, res: express.Response) => {
     let hello = 'Hello World!';
 
     res.send(hello);
-})
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
-})
+});
+
+module.exports = app;
