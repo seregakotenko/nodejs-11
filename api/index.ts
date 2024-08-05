6a0bda860fb3e4b5e7a9b616ccf66e2efa33577d
serhii.kotenko <serega.kotenko@gmail.com>
2024-08-05 17:23:52 +0300

    added test endpoints

--- api/index.ts
+++ api/index.ts
@@ -3,10 +3,26 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+const pets = [{title: 'capybaras'}, {title: 'wolves'}];
+
 app.get("/", (req: express.Request, res: express.Response) => {
     let hello = 'Hello World!';
 
     res.send(hello);
+});
+
+app.get("/pets", (req: express.Request, res: express.Response) => {
+    res.send(pets);
+});
+
+app.get("/pets/:petsTitle", (req: express.Request, res: express.Response) => {
+    let pet = pets.find(p => p.title === req.params.petsTitle);
+
+    if (pet) {
+        res.send(pet);
+    } else {
+        res.status(404).send('Pet Is Not Found');
+    }
 });
 
 app.listen(port, () => {
